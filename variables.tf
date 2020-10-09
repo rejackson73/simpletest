ded66b3140c606f4489af7252a28c27618c7c126
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2020-10-09 13:57:30 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR of the VPC"
+  default     = "192.168.100.0/24"
+}
+
+variable "prefix" {
+  type        = string
+  description = "prefix to identify resources"
+  default     = "my_demo"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "machine instance type"
+  default     = "t3.micro"
+}
+
+variable "aws_key" {
+  type        = string
+  description = "aws security PEM key"
+}
+
+variable "owner_tag" {
+  type        = string
+  description = "infrastructure owner"
+}
+
+variable "ttl_tag" {
+  type        = number
+  description = "infrastructure owner"
+  default     = 72
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "private key"
+}
