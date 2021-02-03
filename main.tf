7fe27db0b22db2c6b066996981bb470e1331b11a
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2021-02-03 18:19:30 -0500

    vault access

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
 provider "aws" {
   version = "~> 2.5"
   region  = var.region
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+  security_token = data.vault_aws_access_credentials.creds.secret_key
 }
+
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  # address = "https://vault.example.net:8200"
+  address = "http://127.0.0.1:8200"
+}
+
+
+data "vault_aws_access_credentials" "creds" {
+  backend = aws2
+  role    = rjackson-vault
+  type    = sts
+  ttl     = "15m"
+}
+
 
 resource aws_vpc "nomad-demo" {
   cidr_block           = var.vpc_cidr
