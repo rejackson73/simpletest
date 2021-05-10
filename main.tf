7ac16352a19c7c5ce6e3eb5e49b9edfabb9c6810
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2021-05-10 09:01:27 -0400

    Removed Private Vault

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 }
-provider "aws" {
-  region  = var.aws_region
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
-}
+// provider "aws" {
+//   region  = var.aws_region
+//   access_key = data.vault_aws_access_credentials.creds.access_key
+//   secret_key = data.vault_aws_access_credentials.creds.secret_key
+// }
 
 data "aws_ami" "an_image" {
   most_recent      = true
