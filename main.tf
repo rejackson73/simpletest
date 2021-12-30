5a10f566bfe9e962338ee2f5f35ffb66f30bc154
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2021-12-30 14:49:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 2.5"
-      region  = var.aws_region
     }
   }
 }
-# provider "aws" {
-#   region  = var.aws_region
+provider "aws" {
+  region  = var.aws_region
 #   access_key = data.vault_aws_access_credentials.creds.access_key
 #   secret_key = data.vault_aws_access_credentials.creds.secret_key
-# }
+}
 
 data "aws_ami" "an_image" {
   most_recent      = true
